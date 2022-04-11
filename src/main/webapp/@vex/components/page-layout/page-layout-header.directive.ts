fa44de6ed9effe57c6ce4e67f158c91af3da48a1
haikhanh9484 <hai.khanh.9484@gmail.com>
2022-04-11 10:17:07 +0700

    first commit

---
+++ src/main/webapp/@vex/components/page-layout/page-layout-header.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[vexPageLayoutHeader],vex-page-layout-header',
+  host: {
+    class: 'vex-page-layout-header',
+  },
+})
+export class PageLayoutHeaderDirective {
+  constructor() {}
+}
---
+++ src/main/webapp/@vex/pipes/strip-html/strip-html.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StripHtmlPipe } from './strip-html.pipe';
+
+@NgModule({
+  declarations: [StripHtmlPipe],
+  imports: [CommonModule],
+  exports: [StripHtmlPipe],
+})
+export class StripHtmlModule {}
